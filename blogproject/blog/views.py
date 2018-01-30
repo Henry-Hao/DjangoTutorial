642a93b84658fd1db272e49c519d775c1e5c7bf8
HenryHao <haoyixian1994@hotmail.com>
2018-01-30 11:53:24 -0500

    import templates and static resources

--- blogproject/blog/views.py
+++ blogproject/blog/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Article
 # Create your views here.
 
 def index(request):
-    return render(request, 'blog/index.html', context={
-        "title": "My Blog Homepage",
-        "welcome": "Welcome to my blog"})
+    articles = Article.objects.all().order_by('-created_time')
+    return render(request, 'blog/index.html', context={'articles':articles})
