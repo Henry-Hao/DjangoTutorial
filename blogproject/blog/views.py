83d99a65cb446b3c0de9a75a05bceb7853ce7be4
HenryHao <haoyixian1994@hotmail.com>
2018-01-31 20:56:31 -0500

    1. add custom tags  "blog.tags.py". 2. add urls for category and tags. 3. change "USE_TZ" in settings.py from True to False

--- blogproject/blog/views.py
+++ blogproject/blog/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
-from .models import Article
+from .models import Article, Category
 import markdown, pygments
 # Create your views here.
 
 def index(request):
     articles = Article.objects.all().order_by('-created_time')
+    return render(request, 'blog/index.html', context={'articles':articles})
+
+def archives(request, year, month):
+    articles = Article.objects.filter(created_time__year=year,created_time__month=month).order_by("-created_time")
+    return render(request, 'blog/index.html', context={'articles':articles})
+
+def category(request, pk):
+    cate = get_object_or_404(Category, pk=pk)
+    articles = Article.objects.filter(category=cate).order_by("-created_time")
     return render(request, 'blog/index.html', context={'articles':articles})
 
 def detail(request, pk):
